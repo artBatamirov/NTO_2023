d390ec0e02830f432684a3d6ca540611af8313c4
user <test@.tru>
2023-11-19 17:28:02 +0300

    add delete and add dialogs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox, QInputDialog, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox, QInputDialog, QWidget, QAbstractItemView
 
 conn = sqlite3.connect("culture_centr.db")
 cur = conn.cursor()
@@ -17,12 +17,16 @@ class App(QMainWindow):
         self.con = QSqlDatabase.addDatabase("QSQLITE")
         self.con.setDatabaseName("culture_centr.db")
 
+        self.tableView_3.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.tableView_3.setSelectionBehavior(QAbstractItemView.SelectRows)
+
         self.load_data()
         self.add_btn_2_2.clicked.connect(self.add_type)
         self.add_btn_3_2.clicked.connect(self.add_type)
         self.reload_btn.clicked.connect(self.load_data)
         self.add_btn_2_1.clicked.connect(self.open_second_form)
         self.add_btn_3_1.clicked.connect(self.open_second_form)
+        self.del_btn_2_1.clicked.connect(self.del_row)
 
 
 
@@ -32,6 +36,7 @@ class App(QMainWindow):
             print(type)
             cur.execute("""INSERT INTO event_type(type) VALUES(?)""", (type,))
             conn.commit()
+            self.load_data()
 
     def load_data(self):
         self.model1 = QSqlTableModel(self)
@@ -62,20 +67,43 @@ class App(QMainWindow):
         self.tableView.setModel(self.model2)
         self.tableView.resizeColumnsToContents()
 
+    def del_row(self):
+        print(1)
+        row = 0
+        for index in sorted(self.tableView_3.selectionModel().selectedRows()):
+            row = index.row()
+        data = []
+        print(2)
+        for col in range(self.tableView_3.model().columnCount() - 1):
+            data.append(self.tableView_3.model().data(self.tableView_3.model().index(row, col + 1)))
+        print(data)
+        print(3)
+
+
+        data = list(map(str, data))
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.resize(100, 100)
+        type = cur.execute("""SELECT type FROM event_type WHERE id = ?""", (data[1], )).fetchone()[0]
+        msg.setText(f'Вы дейсвительно хотите удалить данные?\ndate: {data[0]}\ntype: {type}({data[1]})\ndescription: {data[2]}')
+        msg.setWindowTitle("Удалить данные")
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        retval = msg.exec_()
+        print(msg.clickedButton() == QMessageBox.Cancel)
+
     def open_second_form(self):
-        self.second_form = SecondForm()
-        self.second_form.show()
+            self.second_form = SecondForm(self)
+            self.second_form.show()
 
 class SecondForm(QMainWindow):
-    def __init__(self):
+    def __init__(self, main):
+        self.main = main
         super().__init__()
-        print(1)
         uic.loadUi('add_ui.ui', self)
-        print(2)
         self.initUi()
 
     def initUi(self):
-        # self.setFixedSize(400, 400)
+        self.setFixedSize(500, 200)
         types = cur.execute("""SELECT type FROM event_type""").fetchall()
         print(types)
         self.comboBox.clear()
@@ -95,10 +123,11 @@ class SecondForm(QMainWindow):
         cur.execute("""INSERT INTO event(date, type, description) VALUES(?, ?, ?)""", (date, type, description))
         print(1)
         conn.commit()
+        self.main.load_data()
         self.close()
 
     def cancel(self):
-        self.hide()
+        self.close()
 
 
 
